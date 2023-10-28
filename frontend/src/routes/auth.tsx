17e3bdaca4758716441a455cce857a334feaac26
Mateusz <mateusz4k@outlook.com>
2023-10-28 13:50:46 +0200

    refactor(auth): changed order of form renders

--- frontend/src/routes/auth.tsx
+++ frontend/src/routes/auth.tsx
@@ -8,34 +8,34 @@ const Auth = () => {
 	const { search } = useLocation();
 	const q = search.split('=')[1];
 
-	if (q === 'login') {
+	if (q === 'signup') {
 		return (
-			<AuthCard title='Login'>
+			<AuthCard title='Signup'>
 				<FormControl>
-					<Label title='E-mail' />
-					<Input type='text' placeholder='john@doe.com' />
+					<Label title='E-mail' htmlFor='email' />
+					<Input id='email' type='text' placeholder='john@doe.com' />
 				</FormControl>
 				<FormControl>
-					<Label title='Password' />
-					<Input type='password' />
+					<Label title='Password' htmlFor='password' />
+					<Input type='password' id='password' />
+				</FormControl>
+				<FormControl>
+					<Label title='Confirm Password' htmlFor='confirm' />
+					<Input type='password' id='confirmy' />
 				</FormControl>
 			</AuthCard>
 		);
 	}
 
 	return (
-		<AuthCard title='Signup'>
+		<AuthCard title='Login'>
 			<FormControl>
-				<Label title='E-mail' htmlFor='email' />
-				<Input id='email' type='text' placeholder='john@doe.com' />
+				<Label title='E-mail' />
+				<Input type='text' placeholder='john@doe.com' />
 			</FormControl>
 			<FormControl>
-				<Label title='Password' htmlFor='password' />
-				<Input type='password' id='password' />
-			</FormControl>
-			<FormControl>
-				<Label title='Confirm Password' htmlFor='confirm' />
-				<Input type='password' id='confirmy' />
+				<Label title='Password' />
+				<Input type='password' />
 			</FormControl>
 		</AuthCard>
 	);
