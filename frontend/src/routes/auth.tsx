63293f0b68a054fe19818cf022433257644f422c
Mateusz <mateusz4k@outlook.com>
2023-10-28 13:07:57 +0200

    feat(auth): rendered relevant auth form depends on query params

--- frontend/src/routes/auth.tsx
+++ frontend/src/routes/auth.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { AuthCard } from '../components/auth/Card';
+import { Input } from '../components/auth/Input';
+import { Label } from '../components/auth/Label';
+import { FormControl } from '../components/auth/FormControl';
 
 const Auth = () => {
-	return <div className='m-10 h-20 w-20 bg-red-500'>Auth</div>;
+	const { search } = useLocation();
+	const q = search.split('=')[1];
+
+	if (q === 'login') {
+		return (
+			<AuthCard title='Login'>
+				<FormControl>
+					<Label title='E-mail' />
+					<Input type='text' placeholder='john@doe.com' />
+				</FormControl>
+				<FormControl>
+					<Label title='Password' />
+					<Input type='password' />
+				</FormControl>
+			</AuthCard>
+		);
+	}
+
+	return (
+		<AuthCard title='Signup'>
+			<FormControl>
+				<Label title='E-mail' htmlFor='email' />
+				<Input id='email' type='text' placeholder='john@doe.com' />
+			</FormControl>
+			<FormControl>
+				<Label title='Password' htmlFor='password' />
+				<Input type='password' id='password' />
+			</FormControl>
+			<FormControl>
+				<Label title='Confirm Password' htmlFor='confirm' />
+				<Input type='password' id='confirmy' />
+			</FormControl>
+		</AuthCard>
+	);
 };
 
 export default Auth;
