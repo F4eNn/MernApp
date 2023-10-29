3b3e2482a3f7b4ec67be86c88997d7c1d2fd9325
Mateusz <mateusz4k@outlook.com>
2023-10-29 17:19:10 +0100

    feat(controllers): created post and get todo

---
+++ backend/src/controllers/todo.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import { Todo } from '../models/todo';
+import { CustomError } from '../models/custom-error';
+import { validationResult, Result } from 'express-validator';
+
+export const postTodo = async (req: Request, res: Response, next: NextFunction) => {
+	const errors: Result = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		const { msg, path } = errors.mapped().title;
+		return res.status(422).json({ msg, path });
+	}
+	const todo = new Todo({ title: req.body.title });
+	try {
+		await todo.save();
+		return res.status(201).json({ message: 'Created Todo' });
+	} catch (err) {
+		const error = new CustomError("Can't add todo", 500, 'server issue');
+		next(error);
+	}
+};
+
+export const getTodo = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const todos = await Todo.find();
+		res.status(200).json({ data: todos });
+	} catch (error) {
+		next(new CustomError('Not found any todos', 404));
+	}
+};
