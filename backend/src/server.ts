441aae456be6076bbd12f9d92f1c43b2cc2f46bb
Mateusz <mateusz4k@outlook.com>
2023-10-29 16:20:55 +0100

    feat(server): added body parser

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,21 @@
 import 'dotenv/config';
-import express from 'express';
+import bodyParser from 'body-parser';
 
+import express from 'express';
 import mongoose from 'mongoose';
 
 import { router as todoRouter } from './routes/todo';
 import { handleError } from './middleware/error-handler';
 
-const MONGO_URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.lhyndcn.mongodb.net/todo-app?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.lhyndcn.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
 const server = express();
 
+server.use(bodyParser.json());
+
 server.use(todoRouter);
 
-
-server.use(handleError)
+server.use(handleError);
 
 mongoose
 	.connect(MONGO_URI)
