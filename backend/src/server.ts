10227ad4d3a5d62b5a98e215c322aca3c125cbe6
Mateusz <mateusz4k@outlook.com>
2023-10-29 09:20:40 +0100

    feat(server): setup server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,2 +1,17 @@
-             
-             console.log('say hello!');
+import 'dotenv/config';
+import express from 'express';
+
+import mongoose from 'mongoose';
+
+import {router as authRouter} from './routes/todo';
+
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.lhyndcn.mongodb.net/todo-app?retryWrites=true&w=majority`;
+
+const server = express();
+
+server.use(authRouter);
+
+mongoose
+	.connect(MONGO_URI)
+	.then(result => server.listen(process.env.PORT || 8080))
+	.catch(err => console.log(err));
