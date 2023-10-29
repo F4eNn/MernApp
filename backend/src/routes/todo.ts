60888c57e68c565fb479c7f4753c8c282339fed7
Mateusz <mateusz4k@outlook.com>
2023-10-29 17:17:45 +0100

    feat(routes): added server validation for incoming todo value

--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -1,8 +1,21 @@
 import { Router } from 'express';
 
 import { postTodo, getTodo } from '../controller/todo';
+import { body } from 'express-validator';
 
 export const router = Router();
 
-router.post('/todo', postTodo);
+router.post(
+	'/todo',
+	body('title')
+		.trim()
+		.not()
+		.isEmpty()
+		.withMessage("Can't be empty.")
+		.isLength({ min: 3 })
+		.withMessage('Min. 3 characters.')
+		.isAlphanumeric()
+		.withMessage('Input must contain only digits and letters.'),
+	postTodo,
+);
 router.get('/todo', getTodo);
