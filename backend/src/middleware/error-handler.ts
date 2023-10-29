94a04512627cff1f854359f540c8636509eec794
Mateusz <mateusz4k@outlook.com>
2023-10-29 15:15:07 +0100

    feat(middleware): added new default error msg

--- backend/src/middleware/error-handler.ts
+++ backend/src/middleware/error-handler.ts
@@ -4,7 +4,7 @@ import { CustomError } from '../models/custom-error';
 export const handleError = (err: Error | CustomError, req: Request, res: Response, next: NextFunction) => {
 	let customErr = err;
 	if (!(err instanceof CustomError)) {
-		customErr = new CustomError('test', 404, 'hmm');
+		customErr = new CustomError('We are having troubles', 500, 'Please try again later');
 	}
 	res.status((customErr as CustomError).status).send(customErr);
 };
