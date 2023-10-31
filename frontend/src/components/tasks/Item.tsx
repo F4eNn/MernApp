a2ae9350dbeb97da4ff8387ac83540ded9f94075
Mateusz <mateusz4k@outlook.com>
2023-10-31 21:20:43 +0100

    feat: added new Form and styles when todo is done

--- frontend/src/components/tasks/Item.tsx
+++ frontend/src/components/tasks/Item.tsx
@@ -3,19 +3,26 @@ import { FiEdit } from 'react-icons/fi';
 import { Input } from '../ui/Input';
 import { TodoItem } from '../../types/types';
 import { Form, Link } from 'react-router-dom';
+import { cn } from '../../utils/utils';
 
 export const Item = ({ todo, _id }: TodoItem) => {
+	let isDone;
 	return (
 		<li className='flex justify-between'>
-			<div className='flex items-center gap-6'>
-				<Input
-					type='checkbox'
-					className='form-checkbox hover:bg-gray checked:bg-primary focus:checked:bg-primary hover:checked:bg-primary/70 colors-300 h-10 w-10 cursor-pointer rounded-full'
-				/>
-				<span className='text-xl'>{todo}</span>
-			</div>
+			<Form method='POST' className='flex items-center gap-6'>
+				<input type='hidden' name='todoID' value={_id} />
+				<button type='submit' name='intent' value={'todo-is-done'}>
+					<Input
+						checked={isDone === true}
+						readOnly
+						type='checkbox'
+						className='form-checkbox hover:bg-gray checked:bg-primary focus:checked:bg-primary hover:checked:bg-primary/70 colors-300 h-10 w-10 cursor-pointer rounded-full'
+					/>
+				</button>
+				<span className={cn('text-xl ', isDone && 'italic line-through')}>{todo}</span>
+			</Form>
 			<div className='items-centers flex'>
-				<Link aria-label='edit task' to={`/edit/${_id}`} state={{ todo }} className='colors-300 hover:text-primary p-3'>
+				<Link aria-label='edit task' to={`/edit/${_id}`} state={{ todo }} className='colors-300 git hover:text-primary p-3'>
 					<FiEdit size='25px' />
 				</Link>
 				<Form method='DELETE'>
