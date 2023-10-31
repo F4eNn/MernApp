c09bf707377e94e932e0ddbffab7830d4aef1736
Mateusz <mateusz4k@outlook.com>
2023-10-31 08:31:46 +0100

    feat(types): generate todo item types

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -3,3 +3,11 @@ import { ReactNode } from 'react';
 export type PropsWithChildren = {
 	children: ReactNode;
 };
+
+export type TodoItem = {
+	_id: string;
+	todo: string;
+};
+export type TodoItems = {
+	data: TodoItem[];
+};
