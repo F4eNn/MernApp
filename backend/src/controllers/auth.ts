904dce7d7c4fcbf4f19604c7a6f6a020a8e44396
Mateusz <mateusz4k@outlook.com>
2023-11-01 19:29:34 +0100

    feat: created new controller for login handler

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,13 +1,17 @@
 import type { NextFunction, Request, Response } from 'express';
-import { hash } from 'bcryptjs';
+import { hash, compare } from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 import { User, UserModelType } from '../models/user';
 import { validationResult } from '../utils/utils';
 import { CustomError } from '../models/custom-error';
 
-
 const TOKEN = `${process.env.TOKEN_KEY}`;
+
+const addToken = (email: string, _id: string) => {
+	const token = jwt.sign({ email: email, userID: _id }, TOKEN, { expiresIn: '2h' });
+	return token;
+};
 
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
 	const { email, password } = req.body;
@@ -18,8 +22,7 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
 
 		const newUser: UserModelType = new User({ email, password: hashedPassword });
 		const { email: userEmail, _id } = await newUser.save();
-		const token = jwt.sign({ email: userEmail, userID: _id.toString() }, TOKEN, { expiresIn: '2h' });
-
+		const token = addToken(userEmail, _id.toString());
 		res.status(201).json({ message: 'created user', token });
 	} catch (error) {
 		if (error instanceof CustomError) {
@@ -29,3 +32,23 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
 	}
 };
 
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+	const { email, password } = req.body;
+	try {
+		const user = await User.findOne({ email });
+		if (!user) {
+			throw new CustomError("Account doesn't exist", 404);
+		}
+		const isEqual = await compare(password, user.password);
+		if (!isEqual) {
+			throw new CustomError('Invalid credentials', 401);
+		}
+		const token = addToken(email, user._id.toString());
+		res.status(200).json({ message: 'Successfully login', email, token });
+	} catch (error) {
+		if (error instanceof CustomError) {
+			return next(error);
+		}
+		next(error);
+	}
+};
