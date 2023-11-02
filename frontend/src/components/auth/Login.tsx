580748fcc8ba4d62520ee1c39e73d83170778935
Mateusz <mateusz4k@outlook.com>
2023-11-02 14:25:40 +0100

    feat: implemented features for login route

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,19 +1,44 @@
+import type { FormEvent } from 'react';
+import { useState } from 'react';
 import { Form } from './Form';
 import { FormControl } from '../ui/FormControl';
 import { Label } from '../ui/Label';
 import { Input } from '../ui/Input';
+import { ErrorMsg } from '../ui/ErrorMsg';
+import { authenticate } from '../../utils/auth';
+import { FormValues } from '../../types/types';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
-    return (
-		<Form title='Login'>
+	const [error, setError] = useState('');
+	const navigate = useNavigate();
+
+	const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const formData = new FormData(e.currentTarget);
+		const credentials = Object.fromEntries(formData) as FormValues;
+		const res = await authenticate(credentials, 'login');
+		if (res && !res.ok) {
+			const { message } = await res.json();
+			return setError(message);
+		}
+		const { token, email } = await res?.json();
+		localStorage.setItem('token', token);
+		localStorage.setItem('email', email);
+		return navigate('/');
+	};
+
+	return (
+		<Form title='Login' onSubmit={onSubmit}>
+			<ErrorMsg className='relative top-0' errorMsg={error} />
 			<FormControl>
 				<Label title='E-mail' />
-				<Input type='text' placeholder='john@doe.com' />
+				<Input name='email' type='text' placeholder='john@doe.com' />
 			</FormControl>
 			<FormControl>
 				<Label title='Password' />
-				<Input type='password' />
+				<Input type='password' name='password' />
 			</FormControl>
 		</Form>
 	);
-}
+};
