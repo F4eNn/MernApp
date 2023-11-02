3cd2e6824607dde57ce2d6c91174bbb76b65f069
Mateusz <mateusz4k@outlook.com>
2023-11-02 19:23:10 +0100

    feat: passed bearer token to requests

--- frontend/src/utils/todos.ts
+++ frontend/src/utils/todos.ts
@@ -10,9 +10,10 @@ export const putTodo = async ({
 	isDone?: FormDataEntryValue;
 }) => {
 	try {
+		const token = localStorage.getItem('token');
 		const res = await fetch(`${BACKEND_URL}/todo`, {
 			method: 'PUT',
-			headers: { 'Content-Type': 'application/json' },
+			headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
 			body: JSON.stringify({ todo, todoID, isDone: isDone ? false : true }),
 		});
 		if (!res.ok || res.status !== 201) {
@@ -25,19 +26,21 @@ export const putTodo = async ({
 	}
 };
 export const getTodos = async () => {
+	const token = localStorage.getItem('token');
 	try {
-		const res = await fetch(`${BACKEND_URL}/todo`);
-		const data = res.json();
+		const res = await fetch(`${BACKEND_URL}/todo`, { headers: { Authorization: `Bearer ${token}` } });
+		const data = await res.json();
 		return data;
 	} catch (err) {
 		console.error(err);
 	}
 };
 export const deleteTodo = async (todoID: FormDataEntryValue) => {
+	const token = localStorage.getItem('token');
 	try {
 		const res = await fetch(`${BACKEND_URL}/todo`, {
 			method: 'DELETE',
-			headers: { 'Content-Type': 'application/json' },
+			headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
 			body: JSON.stringify({ todoID }),
 		});
 		return res;
