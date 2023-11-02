343a49a729b4124ece8489edab65e6d6ed86b669
Mateusz <mateusz4k@outlook.com>
2023-11-02 13:24:34 +0100

    feat(auth): implemented logic and includes error handling for register new users

--- frontend/src/components/auth/Signup.tsx
+++ frontend/src/components/auth/Signup.tsx
@@ -1,22 +1,54 @@
+import { useState, FormEvent } from 'react';
+
 import { Form } from './Form';
 import { FormControl } from '../ui/FormControl';
 import { Label } from '../ui/Label';
 import { Input } from '../ui/Input';
+import { FormValues } from '../../types/types';
+import { signup } from '../../utils/auth';
+import { ErrorMsg } from '../ui/ErrorMsg';
+import { useNavigate } from 'react-router-dom';
+
+export type ErrorItem = { msg: string; path: string };
+
+type FormErrorType = {
+	[key in keyof FormValues]: ErrorItem;
+};
 
 export const Signup = () => {
+	const [errors, setErrors] = useState<FormErrorType | null>(null);
+	const navigate = useNavigate();
+
+	const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const formData = new FormData(e.currentTarget);
+		const formValues = Object.fromEntries(formData) as FormValues;
+		const res = await signup(formValues);
+
+		if (res && !res.ok) {
+			const { error } = await res?.json();
+			return setErrors(error);
+		}
+		setErrors(null);
+		return navigate('/');
+	};
+
 	return (
-		<Form title='Signup'>
+		<Form title='Signup' onSubmit={onSubmit}>
 			<FormControl>
 				<Label title='E-mail' htmlFor='email' />
-				<Input id='email' type='text' placeholder='john@doe.com' />
+				<Input error={errors?.email} id='email' name='email' type='text' placeholder='john@doe.com' />
+				{errors && errors.email && <ErrorMsg className='top-0' errorMsg={errors.email.msg} />}
 			</FormControl>
 			<FormControl>
 				<Label title='Password' htmlFor='password' />
-				<Input type='password' id='password' />
+				<Input error={errors?.password} type='password' name='password' id='password' />
+				{errors && errors.password && <ErrorMsg className='top-0' errorMsg={errors.password.msg} />}
 			</FormControl>
 			<FormControl>
 				<Label title='Confirm Password' htmlFor='confirm' />
-				<Input type='password' id='confirmy' />
+				<Input error={errors?.confirmPassword} type='password' id='confirmy' name='confirmPassword' />
+				{errors && errors.confirmPassword && <ErrorMsg className='top-0' errorMsg={errors.confirmPassword.msg} />}
 			</FormControl>
 		</Form>
 	);
