eaf5c883232ec2c80f29bcc22cd8a7b4c6842958
Mateusz <mateusz4k@outlook.com>
2023-11-02 13:21:01 +0100

    feat: added Cn utils for ErrorMsg and error border for input

--- frontend/src/components/ui/ErrorMsg.tsx
+++ frontend/src/components/ui/ErrorMsg.tsx
@@ -1,3 +1,5 @@
-export const ErrorMsg = ({ errorMsg }: { errorMsg: string }) => {
-	return <span className='text-error absolute -top-5 right-1 text-xs'>{errorMsg}</span>;
+import { cn } from "../../utils/utils";
+
+export const ErrorMsg = ({ errorMsg, className }: { errorMsg: string, className?: string }) => {
+	return <span className={cn('text-error absolute -top-5 right-1 text-xs', className)}>{errorMsg}</span>;
 };
--- frontend/src/components/ui/Input.tsx
+++ frontend/src/components/ui/Input.tsx
@@ -1,10 +1,13 @@
 import { InputHTMLAttributes, forwardRef } from 'react';
 import type { ForwardedRef } from 'react';
 import { cn } from '../../utils/utils';
+import { ErrorItem } from '../auth/Signup';
 
-type InpuProps = InputHTMLAttributes<HTMLInputElement>;
+type InpuProps = InputHTMLAttributes<HTMLInputElement> & {
+	error?: ErrorItem | undefined;
+};
 
-export const Input = forwardRef(({ className, ...rest }: InpuProps, ref: ForwardedRef<HTMLInputElement>) => {
+export const Input = forwardRef(({ className, error, ...rest }: InpuProps, ref: ForwardedRef<HTMLInputElement>) => {
 	return (
 		<input
 			ref={ref}
@@ -12,6 +15,7 @@ export const Input = forwardRef(({ className, ...rest }: InpuProps, ref: Forward
 			className={cn(
 				'form-input focus:border-primary  w-full rounded-md px-3 py-1 focus:outline-0 focus:ring-0',
 				className,
+				error && 'border-error',
 			)}
 		></input>
 	);
